dc1758c03f6056709a0ce84673dce20f9a3699de
Markus Nigbur <markus@mnigbur.de>
2019-04-22 17:11:42 +0200

    Cleaned Branch

--- shell_scripts/HAUpdate.sh
+++
@@ -1,8 +0,0 @@
-cd /home/homeassistant/.homeassistant
-source /srv/homeassistant/homeassistant_venv/bin/activate
-
-echo "Processing update"
-pip3 install --upgrade homeassistant
-
-hass --script check_config
-exit
